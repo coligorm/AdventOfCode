ae984f43b7b2d80271187177cb6164e8aa06f0b5
coligorm <colinrhysgorman@gmail.com>
2024-12-15 12:53:28 +1300

    cleaned day-2 by adding main and functions

--- Advent_of_Code/Day_2/Day_2.py
+++ Advent_of_Code/Day_2/Day_2.py
@@ -1,42 +1,49 @@
 import sys
-inFile = sys.argv[1]
-
-list_of_lists = []
-with open(inFile,'r') as file:
-    for line in file:
-        line = line.split()
-        if line:
-            line = [int(i) for i in line]
-            list_of_lists.append(line)
-
-print("Input:",list_of_lists)
-
 
 def compare(x,y):
-    if 0 <abs((x - y)) <= 3 :
+    if 0 < abs((x - y)) <= 3 :
         return True
-    
+
 def is_increasing_or_decreasing(l):
-    if l == sorted(l) or l == sorted(l, reverse = True):
-        return True
+    return l == sorted(l) or l == sorted(l, reverse = True)
+
+def is_safe(l):
+    safe = False
+    i = 0
+    while i < len(l)-1:
+        if compare(l[i], l[i+1]) and is_increasing_or_decreasing(l):
+            safe = True
+        else:
+            safe = False
+            break
+        
+        i+=1
+
+    return safe
 
 
-safe_count = 0
+def main():
+    safe_count = 0
 
-for i in list_of_lists:
-    if is_increasing_or_decreasing(i):
-        j = 0
-        safe = False
-        while j < len(i)-1:
-            if compare(i[j],i[j+1]):
-                safe = True
-                j+=1
-            else:
-                safe = False
-                break
-                
-        if safe:
-            safe_count += 1
-        print(i,":",safe)
+    for i in list_of_lists:
+        if is_safe(i):
+            print(i,":",is_safe(i))
+            safe_count+=1
 
-print(safe_count," reports are safe")
+    print(safe_count," reports are safe")
+
+
+
+if __name__ == "__main__":
+    # inFile = sys.argv[1]
+    list_of_lists = []
+    with open('test.txt','r') as file:
+        for line in file:
+            line = line.split()
+            if line:
+                line = [int(i) for i in line]
+                list_of_lists.append(line)
+
+    print("Input:",list_of_lists)
+
+    main()
