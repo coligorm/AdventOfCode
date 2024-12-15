27c14a204bb35db341d22f1155a435bb1358a269
coligorm <colinrhysgorman@gmail.com>
2024-12-15 19:03:52 +1300

    day-3 completed

--- Advent_of_Code/Day_3/Day_3.py
+++ Advent_of_Code/Day_3/Day_3.py
@@ -4,30 +4,42 @@ import math
 def regex(reg, string):
     return re.findall(reg, string)
 
+def mul(instruction):
+    digits = regex(extract_nums,instruction)
+    nums = [int(x) for x in digits]
+    prod = math.prod(nums)
+
+    return prod
+
 def main():
     print("corrupted file =",txt,"\n")
     
     total = 0
-
-    findMul = r"mul\(\d*\,\d*\)"
-    extract_nums = r"\d+"
+    toggle = True
     
-    instructions = regex(findMul,txt)
-    count = 0
+    instructions = regex(findInstructions,txt)
+    print(instructions,"\n")
+    
     for i in instructions:
-        count +=1
-        digits = regex(extract_nums,i)
-        nums = [int(x) for x in digits]
+        if toggle and "mul" in i:
+            ans = mul(i)
+            total += ans
+        
+        match i:
+            case "don't()":
+                toggle = False   
+            case "do()":
+                toggle = True
 
-        prod = math.prod(nums)
-        total += prod
-    print(total)
-
+    print("Total =",total)
 
 
 if __name__ == "__main__":
     # inFile = sys.argv[1]
     with open('input.txt','r') as file:
         txt = file.read()
+
+    findInstructions = r"(?:mul\(\d*\,\d*\))|(?:do\(\))|(?:don\'t\(\))"
+    extract_nums = r"\d+"
 
     main()
